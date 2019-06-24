210db60b98fc09ddc858698b1760ab54558ebae6
TZ <t.zmitruk@gmail.com>
2019-06-24 09:02:02 +0300

    first

---
+++ j.a.snake/j.a.snake/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace j.a.snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell World");
+            Console.ReadLine();
+        }
+    }
+}
